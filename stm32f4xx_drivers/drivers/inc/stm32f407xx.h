32d2f92e6c9e8b3f8160a8fae9068543ed76b154
Shriyans S Sahoo <shriyans.s.sahoo@gmail.com>
2026-03-26 18:52:53 +0530

    learniing driver_2

--- stm32f4xx_drivers/drivers/inc/stm32f407xx.h
+++ stm32f4xx_drivers/drivers/inc/stm32f407xx.h
@@ -8,6 +8,10 @@
 #ifndef INC_STM32F407XX_H_
 #define INC_STM32F407XX_H_
 
-
+#define FLASH_BASEADDR          0x08000000U
+#define SRAM1_BASEADDR          0x20000000U
+#define SRAM2_BASEADDR
+#define ROM
+#define SRAM                    SRAM1_BASEADDR
 
 #endif /* INC_STM32F407XX_H_ */

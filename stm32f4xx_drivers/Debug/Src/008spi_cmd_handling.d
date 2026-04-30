56c9d5898524b8c95d9c86077366940030d28e20
Shriyans S Sahoo <shriyans.s.sahoo@gmail.com>
2026-04-30 18:10:12 +0530

    008_cmd tested

--- stm32f4xx_drivers/Debug/Src/008spi_cmd_handling.d
+++ stm32f4xx_drivers/Debug/Src/008spi_cmd_handling.d
@@ -1,4 +1,4 @@
-Src/007spi_txonly_arduino.o: ../Src/007spi_txonly_arduino.c \
+Src/008spi_cmd_handling.o: ../Src/008spi_cmd_handling.c \
  C:/Users/HP/OneDrive/Desktop/Embedded-C/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx.h \
  C:/Users/HP/OneDrive/Desktop/Embedded-C/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx_gpio_driver.h \
  C:/Users/HP/OneDrive/Desktop/Embedded-C/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx.h \

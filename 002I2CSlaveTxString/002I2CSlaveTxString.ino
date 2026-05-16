22e4045d50134dcc899c4c48f6e7363535202693
Shriyans S Sahoo <shriyans.s.sahoo@gmail.com>
2026-05-17 01:22:55 +0530

    002I2CTx

---
+++ 002I2CSlaveTxString/002I2CSlaveTxString.ino
@@ -0,0 +1,21 @@
+#include <Wire.h>
+
+int LED=13;
+uint8_t active_command=0xff, led_status=0;
+char name_msg[32]="THis is arduino Uno Board\r\n";
+
+uint16_t device_id=0xFF45;
+
+#define SLAVE_ADDR 0x68
+uint8_t get_len_of_data(void)
+{
+  return (uint8_t)strlen(name_msg);
+}
+void setup(){
+  pinMode(LED, OUTPUT);
+  Wire.begin(SLAVE_ADDR);
+
+  Wire.onReceive(receiveEvent);
+  Wire.onRecquest(requestEvent);
+
+}

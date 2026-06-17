f860204183d950c94b2dcc96aec11383cbec6f7b
Shriyans S Sahoo <shriyans.s.sahoo@gmail.com>
2026-06-17 10:07:36 +0530

    implementing master slave usinf arduino

---
+++ 003I2CMasterRxString.ino/003I2CMasterRxString.ino.ino
@@ -0,0 +1,18 @@
+#include <Wire.h>
+
+int LED=13;
+
+uint8_t rcv_buf[32];
+int data_len=0;
+
+#define SLAVE ADDR 0x68
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}

269fff1f27b21eab8f292ba3db6ba2d51d7887cc
Shriyans S Sahoo <shriyans.s.sahoo@gmail.com>
2026-06-17 10:12:23 +0530

    new file coding for master slave config

--- stm32f4xx_drivers/Debug/Src/013i2c_master_tx_string.d
+++ stm32f4xx_drivers/Debug/Src/013i2c_master_tx_string.d
@@ -1,4 +1,4 @@
-Src/012i2c_master_rx_testingIT.o: ../Src/012i2c_master_rx_testingIT.c \
+Src/013i2c_master_tx_string.o: ../Src/013i2c_master_tx_string.c \
  C:/Users/HP/OneDrive/Desktop/Embedded-C/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx.h \
  C:/Users/HP/OneDrive/Desktop/Embedded-C/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx_gpio_driver.h \
  C:/Users/HP/OneDrive/Desktop/Embedded-C/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx.h \
--- stm32f4xx_drivers/Debug/Src/subdir.mk
+++ stm32f4xx_drivers/Debug/Src/subdir.mk
@@ -5,17 +5,17 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Src/012i2c_master_rx_testingIT.c \
+../Src/013i2c_master_tx_string.c \
 ../Src/syscalls.c \
 ../Src/sysmem.c 
 
 OBJS += \
-./Src/012i2c_master_rx_testingIT.o \
+./Src/013i2c_master_tx_string.o \
 ./Src/syscalls.o \
 ./Src/sysmem.o 
 
 C_DEPS += \
-./Src/012i2c_master_rx_testingIT.d \
+./Src/013i2c_master_tx_string.d \
 ./Src/syscalls.d \
 ./Src/sysmem.d 
 
@@ -27,7 +27,7 @@ Src/%.o Src/%.su Src/%.cyclo: ../Src/%.c Src/subdir.mk
 clean: clean-Src
 
 clean-Src:
-	-$(RM) ./Src/012i2c_master_rx_testingIT.cyclo ./Src/012i2c_master_rx_testingIT.d ./Src/012i2c_master_rx_testingIT.o ./Src/012i2c_master_rx_testingIT.su ./Src/syscalls.cyclo ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.cyclo ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su
+	-$(RM) ./Src/013i2c_master_tx_string.cyclo ./Src/013i2c_master_tx_string.d ./Src/013i2c_master_tx_string.o ./Src/013i2c_master_tx_string.su ./Src/syscalls.cyclo ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.cyclo ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su
 
 .PHONY: clean-Src
 

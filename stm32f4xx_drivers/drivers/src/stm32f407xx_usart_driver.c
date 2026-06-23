1495770b6bf71619a206755791184f81a48dfe48
Shriyans S Sahoo <shriyans.s.sahoo@gmail.com>
2026-06-23 10:10:48 +0530

    added recieve data API

--- stm32f4xx_drivers/drivers/src/stm32f407xx_usart_driver.c
+++ stm32f4xx_drivers/drivers/src/stm32f407xx_usart_driver.c
@@ -258,3 +258,80 @@ void USART_SendData(USART_Handle_t *pUSARTHandle, uint8_t *pTxBuffer, uint32_t L
 	//Implement the code to wait till TC flag is set in the SR
 	while( ! USART_GetFlagStatus(pUSARTHandle->pUSARTx,USART_FLAG_TC));
 }
+
+
+/*********************************************************************
+ * @fn      		  - USART_ReceiveData
+ *
+ * @brief             -
+ *
+ * @param[in]         -
+ * @param[in]         -
+ * @param[in]         -
+ *
+ * @return            -
+ *
+ * @Note              -
+
+ */
+
+void USART_ReceiveData(USART_Handle_t *pUSARTHandle, uint8_t *pRxBuffer, uint32_t Len)
+{
+   //Loop over until "Len" number of bytes are transferred
+	for(uint32_t i = 0 ; i < Len; i++)
+	{
+		//Implement the code to wait until RXNE flag is set in the SR
+		while(! USART_GetFlagStatus(pUSARTHandle->pUSARTx,USART_FLAG_RXNE));
+
+		//Check the USART_WordLength to decide whether we are going to receive 9bit of data in a frame or 8 bit
+		if(pUSARTHandle->USART_Config.USART_WordLength == USART_WORDLEN_9BITS)
+		{
+			//We are going to receive 9bit data in a frame
+
+			//Now, check are we using USART_ParityControl control or not
+			if(pUSARTHandle->USART_Config.USART_ParityControl == USART_PARITY_DISABLE)
+			{
+				//No parity is used , so all 9bits will be of user data
+
+				//read only first 9 bits so mask the DR with 0x01FF
+				*((uint16_t*) pRxBuffer) = (pUSARTHandle->pUSARTx->DR  & (uint16_t)0x01FF);
+
+				//Now increment the pRxBuffer two times
+				pRxBuffer++;
+				pRxBuffer++;
+			}
+			else
+			{
+				//Parity is used, so 8bits will be of user data and 1 bit is parity
+				 *pRxBuffer = (pUSARTHandle->pUSARTx->DR  & (uint8_t)0xFF);
+				 pRxBuffer++;
+			}
+		}
+		else
+		{
+			//We are going to receive 8bit data in a frame
+
+			//Now, check are we using USART_ParityControl control or not
+			if(pUSARTHandle->USART_Config.USART_ParityControl == USART_PARITY_DISABLE)
+			{
+				//No parity is used , so all 8bits will be of user data
+
+				//read 8 bits from DR
+				 *pRxBuffer = (uint8_t) (pUSARTHandle->pUSARTx->DR  & (uint8_t)0xFF);
+			}
+
+			else
+			{
+				//Parity is used, so , 7 bits will be of user data and 1 bit is parity
+
+				//read only 7 bits , hence mask the DR with 0X7F
+				 *pRxBuffer = (uint8_t) (pUSARTHandle->pUSARTx->DR  & (uint8_t)0x7F);
+
+			}
+
+			//Now , increment the pRxBuffer
+			pRxBuffer++;
+		}
+	}
+
+}

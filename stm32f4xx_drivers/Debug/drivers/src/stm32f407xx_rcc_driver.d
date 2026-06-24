ba07dee43016b38da9cddc8b1e0321b5faee792f
Shriyans S Sahoo <shriyans.s.sahoo@gmail.com>
2026-06-24 10:09:11 +0530

    improved RCC code

--- stm32f4xx_drivers/Debug/drivers/src/stm32f407xx_rcc_driver.d
+++ stm32f4xx_drivers/Debug/drivers/src/stm32f407xx_rcc_driver.d
@@ -1,4 +1,16 @@
 drivers/src/stm32f407xx_rcc_driver.o: \
  ../drivers/src/stm32f407xx_rcc_driver.c \
- ../drivers/src/stm32f407xx_rcc_driver.c
-../drivers/src/stm32f407xx_rcc_driver.c:
+ C:/Users/HP/OneDrive/Desktop/Embedded-C/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx_rcc_driver.h \
+ C:/Users/HP/OneDrive/Desktop/Embedded-C/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx.h \
+ C:/Users/HP/OneDrive/Desktop/Embedded-C/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx_gpio_driver.h \
+ C:/Users/HP/OneDrive/Desktop/Embedded-C/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx_spi_driver.h \
+ C:/Users/HP/OneDrive/Desktop/Embedded-C/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx_i2c_driver.h \
+ C:/Users/HP/OneDrive/Desktop/Embedded-C/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx_usart_driver.h \
+ C:/Users/HP/OneDrive/Desktop/Embedded-C/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx_rcc_driver.h
+C:/Users/HP/OneDrive/Desktop/Embedded-C/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx_rcc_driver.h:
+C:/Users/HP/OneDrive/Desktop/Embedded-C/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx.h:
+C:/Users/HP/OneDrive/Desktop/Embedded-C/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx_gpio_driver.h:
+C:/Users/HP/OneDrive/Desktop/Embedded-C/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx_spi_driver.h:
+C:/Users/HP/OneDrive/Desktop/Embedded-C/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx_i2c_driver.h:
+C:/Users/HP/OneDrive/Desktop/Embedded-C/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx_usart_driver.h:
+C:/Users/HP/OneDrive/Desktop/Embedded-C/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx_rcc_driver.h:

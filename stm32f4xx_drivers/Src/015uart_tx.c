c8383e7063d8cfd7398b18308836fcb0e2f59b53
Shriyans S Sahoo <shriyans.s.sahoo@gmail.com>
2026-06-25 11:23:59 +0530

    coding to send data to arduino_2

--- stm32f4xx_drivers/Src/015uart_tx.c
+++ stm32f4xx_drivers/Src/015uart_tx.c
@@ -25,3 +25,54 @@ void USART2_Init(void)
 	usart2_handle.USART_Config.USART_ParityControl = USART_PARITY_DISABLE;
 	USART_Init(&usart2_handle);
 }
+
+
+
+void 	USART2_GPIOInit(void)
+{
+	GPIO_Handle_t usart_gpios;
+
+	usart_gpios.pGPIOx = GPIOA;
+	usart_gpios.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_ALTFN;
+	usart_gpios.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+	usart_gpios.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PIN_PU;
+	usart_gpios.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	usart_gpios.GPIO_PinConfig.GPIO_PinAltFunMode =7;
+
+	//USART2 TX
+	usart_gpios.GPIO_PinConfig.GPIO_PinNumber  = GPIO_PIN_NO_2;
+	GPIO_Init(&usart_gpios);
+
+	//USART2 RX
+	usart_gpios.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_3;
+	GPIO_Init(&usart_gpios);
+
+
+}
+
+void GPIO_ButtonInit(void)
+{
+	GPIO_Handle_t GPIOBtn,GpioLed;
+
+	//this is btn gpio configuration
+	GPIOBtn.pGPIOx = GPIOA;
+	GPIOBtn.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_0;
+	GPIOBtn.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_IN;
+	GPIOBtn.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	GPIOBtn.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+
+	GPIO_Init(&GPIOBtn);
+
+	//this is led gpio configuration
+	GpioLed.pGPIOx = GPIOD;
+	GpioLed.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_12;
+	GpioLed.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	GpioLed.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	GpioLed.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_OD;
+	GpioLed.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
+
+	GPIO_PeriClockControl(GPIOD,ENABLE);
+
+	GPIO_Init(&GpioLed);
+
+}

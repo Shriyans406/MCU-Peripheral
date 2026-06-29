ef5b0af164162d2ba9e6712242c4ac2f318346b3
Shriyans S Sahoo <shriyans.s.sahoo@gmail.com>
2026-06-29 10:55:48 +0530

    started 016 coding

--- stm32f4xx_drivers/Debug/Src/subdir.mk
+++ stm32f4xx_drivers/Debug/Src/subdir.mk
@@ -5,17 +5,17 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Src/015uart_tx.c \
+../Src/16uart_case.c \
 ../Src/syscalls.c \
 ../Src/sysmem.c 
 
 OBJS += \
-./Src/015uart_tx.o \
+./Src/16uart_case.o \
 ./Src/syscalls.o \
 ./Src/sysmem.o 
 
 C_DEPS += \
-./Src/015uart_tx.d \
+./Src/16uart_case.d \
 ./Src/syscalls.d \
 ./Src/sysmem.d 
 
@@ -27,7 +27,7 @@ Src/%.o Src/%.su Src/%.cyclo: ../Src/%.c Src/subdir.mk
 clean: clean-Src
 
 clean-Src:
-	-$(RM) ./Src/015uart_tx.cyclo ./Src/015uart_tx.d ./Src/015uart_tx.o ./Src/015uart_tx.su ./Src/syscalls.cyclo ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.cyclo ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su
+	-$(RM) ./Src/16uart_case.cyclo ./Src/16uart_case.d ./Src/16uart_case.o ./Src/16uart_case.su ./Src/syscalls.cyclo ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.cyclo ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su
 
 .PHONY: clean-Src
 
--- stm32f4xx_drivers/Src/16uart_case.c
+++ stm32f4xx_drivers/Src/16uart_case.c
@@ -6,3 +6,31 @@
  */
 
 
+#include<stdio.h>
+#include<string.h>
+#include "stm32f407xx.h"
+
+
+//we have 3 different messages that we transmit to arduino
+//you can by all means add more messages
+char *msg[3] = {"hihihihihihi123", "Hello How are you ?" , "Today is Monday !"};
+
+//reply from arduino will be stored here
+char rx_buf[1024] ;
+
+USART_Handle_t usart2_handle;
+
+
+//This flag indicates reception completion
+uint8_t rxCmplt = RESET;
+
+uint8_t g_data = 0;
+
+extern void initialise_monitor_handles();
+
+
+
+int main(void)
+{
+}
+

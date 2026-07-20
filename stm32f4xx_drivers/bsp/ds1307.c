5ba187f08e6af0cf5477c0b6b50c82b0297c49ac
Shriyans S Sahoo <shriyans.s.sahoo@gmail.com>
2026-07-20 10:35:38 +0530

    starting ds1307 source

--- stm32f4xx_drivers/bsp/ds1307.c
+++ stm32f4xx_drivers/bsp/ds1307.c
@@ -6,3 +6,30 @@
  */
 
 
+#include "ds1307.h"
+
+
+uint8_t ds1307_init(void)
+{
+
+}
+
+void ds1307_set_current_time(RTC_time_t *rtc_time)
+{
+
+}
+
+void ds1307_set_current_date(RTC_date_t *rtc_date)
+{
+
+}
+
+void ds1307_get_current_time(RTC_time_t *rtc_time)
+{
+
+}
+
+void ds1307_get_current_date(RTC_date_t *rtc_date)
+{
+
+}

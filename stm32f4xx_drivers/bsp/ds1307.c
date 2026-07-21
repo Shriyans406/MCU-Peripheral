41418d110034cd4b92d8aefe495e3c27975dd083
Shriyans S Sahoo <shriyans.s.sahoo@gmail.com>
2026-07-22 01:23:31 +0530

    ds1307 i2c write error

--- stm32f4xx_drivers/bsp/ds1307.c
+++ stm32f4xx_drivers/bsp/ds1307.c
@@ -5,16 +5,30 @@
  *      Author: HP
  */
 
+#include<stdint.h>
+#include<string.h>
 
 #include "ds1307.h"
 
 
 static void ds1307_i2c_pin_config(void);
+I2C_Handle_t g_ds1307I2cHandle;
 
 uint8_t ds1307_init(void)
 {
 	//1. init the i2c pins
 	ds1307_i2c_pin_config();
+
+	//2. initialize the i2c peripheral
+	ds1307_i2c_config();
+
+	//3. Enable the I2C peripheral
+	I2C_PeripheralControl(DS1307_I2C, ENABLE);
+
+	//4. Make clock halt = 0;
+	ds1307_write(0x00,DS1307_ADDR_SEC);
+
+
 
 }
 
@@ -68,4 +82,22 @@ static void ds1307_i2c_pin_config(void)
 
 	GPIO_Init(&i2c_scl);
 
+}
+
+
+void ds1307_i2c_config(void)
+{
+	g_ds1307I2cHandle.pI2Cx = DS1307_I2C;
+	g_ds1307I2cHandle.I2C_Config.I2C_AckControl = I2C_ACK_ENABLE;
+	g_ds1307I2cHandle.I2C_Config.I2C_SCLSpeed = DS1307_I2C_SPEED;
+	I2C_Init(&g_ds1307I2cHandle);
+}
+
+
+void ds1307_write(uint8_t value,uint8_t reg_addr)
+{
+	uint8_t tx[2];
+	tx[0] = reg_addr;
+	tx[1] = value;
+	I2C_MasterSendData(&g_ds1307I2cHandle, tx, 2, DS1307_I2C_ADDRESS, 0);
 }

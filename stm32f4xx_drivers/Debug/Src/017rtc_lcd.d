9ff6960d420e997d7d6e7b99ea3ecfce0e1bbf97
Shriyans S Sahoo <shriyans.s.sahoo@gmail.com>
2026-07-24 01:22:48 +0530

    lcd source file coding

--- stm32f4xx_drivers/Debug/Src/017rtc_lcd.d
+++ stm32f4xx_drivers/Debug/Src/017rtc_lcd.d
@@ -1,1 +1,19 @@
-Src/017rtc_lcd.o: ../Src/017rtc_lcd.c
+Src/017rtc_lcd.o: ../Src/017rtc_lcd.c \
+ C:/Users/HP/OneDrive/Desktop/Embedded-C/MCU1-Course/MCU1/stm32f4xx_drivers/bsp/ds1307.h \
+ C:/Users/HP/OneDrive/Desktop/Embedded-C/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx.h \
+ C:/Users/HP/OneDrive/Desktop/Embedded-C/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx_gpio_driver.h \
+ C:/Users/HP/OneDrive/Desktop/Embedded-C/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx.h \
+ C:/Users/HP/OneDrive/Desktop/Embedded-C/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx_spi_driver.h \
+ C:/Users/HP/OneDrive/Desktop/Embedded-C/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx_i2c_driver.h \
+ C:/Users/HP/OneDrive/Desktop/Embedded-C/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx_usart_driver.h \
+ C:/Users/HP/OneDrive/Desktop/Embedded-C/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx_rcc_driver.h \
+ C:/Users/HP/OneDrive/Desktop/Embedded-C/MCU1-Course/MCU1/stm32f4xx_drivers/bsp/lcd.h
+C:/Users/HP/OneDrive/Desktop/Embedded-C/MCU1-Course/MCU1/stm32f4xx_drivers/bsp/ds1307.h:
+C:/Users/HP/OneDrive/Desktop/Embedded-C/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx.h:
+C:/Users/HP/OneDrive/Desktop/Embedded-C/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx_gpio_driver.h:
+C:/Users/HP/OneDrive/Desktop/Embedded-C/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx.h:
+C:/Users/HP/OneDrive/Desktop/Embedded-C/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx_spi_driver.h:
+C:/Users/HP/OneDrive/Desktop/Embedded-C/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx_i2c_driver.h:
+C:/Users/HP/OneDrive/Desktop/Embedded-C/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx_usart_driver.h:
+C:/Users/HP/OneDrive/Desktop/Embedded-C/MCU1-Course/MCU1/stm32f4xx_drivers/drivers/inc/stm32f407xx_rcc_driver.h:
+C:/Users/HP/OneDrive/Desktop/Embedded-C/MCU1-Course/MCU1/stm32f4xx_drivers/bsp/lcd.h:
--- stm32f4xx_drivers/Src/017rtc_lcd.c
+++ stm32f4xx_drivers/Src/017rtc_lcd.c
@@ -6,8 +6,46 @@
  */
 
 
+#include<stdio.h>
+#include "ds1307.h"
+
+#include "lcd.h"
+
 int main(void)
 {
     // Your code here
+
+	RTC_time_t current_time;
+	RTC_date_t current_date;
+
+	printf("RTC test\n");
+
+	if(ds1307_init()){
+			printf("RTC init has failed\n");
+			while(1);
+	}
+
+	current_date.day = FRIDAY;
+	current_date.date = 15;
+	current_date.month = 1;
+	current_date.year = 21;
+
+	current_time.hours = 11;
+	current_time.minutes = 59;
+	current_time.seconds = 30;
+	current_time.time_format = TIME_FORMAT_12HRS_PM;
+
+	ds1307_set_current_date(&current_date);
+	ds1307_set_current_time(&current_time);
+
+	ds1307_get_current_time(&current_time);
+	ds1307_get_current_date(&current_date);
+
+	char *am_pm;
+	if(current_time.time_format != TIME_FORMAT_24HRS){
+		am_pm = (current_time.time_format) ? "PM" : "AM";
+		printf("Current time = %s %s\n",time_to_string(&current_time),am_pm);
+	}
+
     return 0;
 }

b61dfae05a2a2662c377c9cdc424fe906b4da592
Shriyans S Sahoo <shriyans.s.sahoo@gmail.com>
2026-08-16 01:40:02 +0530

    started new gpio project

---
+++ gpio/Debug/Drivers/hal_gpio_driver.d
@@ -0,0 +1,1 @@
+Drivers/hal_gpio_driver.o: ../Drivers/hal_gpio_driver.c
---
+++ gpio/Drivers/hal_gpio_driver.h
@@ -0,0 +1,13 @@
+/*
+ * hal_gpio_driver.h
+ *
+ *  Created on: Aug 16, 2026
+ *      Author: HP
+ */
+
+#ifndef HAL_GPIO_DRIVER_H_
+#define HAL_GPIO_DRIVER_H_
+
+
+
+#endif /* HAL_GPIO_DRIVER_H_ */

a4317a879e04c25a2eeb8419f8ba4a0259225301
Shriyans S Sahoo <shriyans.s.sahoo@gmail.com>
2026-08-20 17:12:19 +0530

    sorted the error

--- gpio/Debug/Drivers/src/hal_gpio_driver.d
+++ gpio/Debug/Drivers/src/hal_gpio_driver.d
@@ -1,4 +1,16 @@
 Drivers/src/hal_gpio_driver.o: ../Drivers/src/hal_gpio_driver.c \
- ../Drivers/inc/hal_gpio_driver.h ../Drivers/inc/stm32f407xx.h
+ ../Drivers/inc/hal_gpio_driver.h ../Drivers/inc/stm32f407xx.h \
+ ../Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f072xb.h \
+ ../Drivers/CMSIS/Include/core_cm0.h \
+ ../Drivers/CMSIS/Include/cmsis_version.h \
+ ../Drivers/CMSIS/Include/cmsis_compiler.h \
+ ../Drivers/CMSIS/Include/cmsis_gcc.h \
+ ../Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h
 ../Drivers/inc/hal_gpio_driver.h:
 ../Drivers/inc/stm32f407xx.h:
+../Drivers/CMSIS/Device/ST/STM32F0xx/Include/stm32f072xb.h:
+../Drivers/CMSIS/Include/core_cm0.h:
+../Drivers/CMSIS/Include/cmsis_version.h:
+../Drivers/CMSIS/Include/cmsis_compiler.h:
+../Drivers/CMSIS/Include/cmsis_gcc.h:
+../Drivers/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h:

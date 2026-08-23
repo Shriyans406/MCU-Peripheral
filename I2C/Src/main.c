366187e8c72139ddd0aa8fdcad1754906f4f6bdc
Shriyans S Sahoo <shriyans.s.sahoo@gmail.com>
2026-08-23 09:19:11 +0530

    staretd main.c

--- I2C/Src/main.c
+++ I2C/Src/main.c
@@ -17,6 +17,37 @@
  */
 
 #include <stdint.h>
+#include <string.h>
+
+//#include "hal_i2c_driver.h"
+#include "hal_gpio_driver.h"
+#include "i2c_main.h"
+#include "led.h"
+
+/* Uncomment to build as I2C master */
+//#define I2C_MASTER_MODE_EN
+
+#define SLAVE_OWN_ADDRESS      (uint8_t)0x53
+#define SLAVE_ADDRESS_READ     (uint8_t)0xA7
+#define SLAVE_ADDRESS_WRITE    (uint8_t)0xA6
+
+#define GENERAL_CALL_ADDRESS   (uint8_t)0x00
+
+#define MASTER_WRITE_CMD       0xC1
+#define MASTER_READ_CMD        0xC2
+
+#define READ_LEN    5
+#define WRITE_LEN   5
+
+uint8_t master_tx_buffer[WRITE_LEN] = {0xa5, 0x55, 0xa5, 0x55, 0xb0};
+uint8_t master_rx_buffer[READ_LEN];
+uint8_t slave_tx_buffer[WRITE_LEN]  = {0xa5, 0x55, 0xa5, 0x55, 0xc0};
+uint8_t slave_rx_buffer[READ_LEN];
+
+uint8_t master_write_req;
+uint8_t master_read_req;
+
+uint8_t slave_rcv_cmd;
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."

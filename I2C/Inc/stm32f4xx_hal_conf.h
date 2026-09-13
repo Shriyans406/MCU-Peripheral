4de1e6daf7d0aa8b1d8ebce414cdd379391b11e7
Shriyans S Sahoo <shriyans.s.sahoo@gmail.com>
2026-09-13 09:11:23 +0530

    defining macros_2

--- I2C/Inc/stm32f4xx_hal_conf.h
+++ I2C/Inc/stm32f4xx_hal_conf.h
@@ -126,4 +126,46 @@
  #define  INSTRUCTION_CACHE_ENABLE     1
  #define  DATA_CACHE_ENABLE            1
 
+ /* ########################## Assert Selection ############################## */
+ /**
+   * @brief Uncomment the line below to expanse the "assert_param" macro in the
+   *        HAL drivers code
+   */
+ /* #define USE_FULL_ASSERT    1 */
+
+ /* ################## Ethernet peripheral configuration ##################### */
+
+ /* Section 1 : Ethernet peripheral configuration */
+
+ /* MAC ADDRESS: MAC_ADDR0:MAC_ADDR1:MAC_ADDR2:MAC_ADDR3:MAC_ADDR4:MAC_ADDR5 */
+ #define MAC_ADDR0   2
+ #define MAC_ADDR1   0
+ #define MAC_ADDR2   0
+ #define MAC_ADDR3   0
+ #define MAC_ADDR4   0
+ #define MAC_ADDR5   0
+
+ /* Definition of the Ethernet driver buffers size and count */
+ #define ETH_RX_BUF_SIZE                ETH_MAX_PACKET_SIZE /* buffer size for receive               */
+ #define ETH_TX_BUF_SIZE                ETH_MAX_PACKET_SIZE /* buffer size for transmit              */
+ #define ETH_RXBUFNB                    ((uint32_t)4)       /* 4 Rx buffers of size ETH_RX_BUF_SIZE  */
+ #define ETH_TXBUFNB                    ((uint32_t)4)       /* 4 Tx buffers of size ETH_TX_BUF_SIZE  */
+
+ /* Section 2: PHY configuration section */
+
+ /* DP83848 PHY Address*/
+ #define DP83848_PHY_ADDRESS             0x01
+ /* PHY Reset delay these values are based on a 1 ms Systick interrupt*/
+ #define PHY_RESET_DELAY                 ((uint32_t)0x000000FF)
+ /* PHY Configuration delay */
+ #define PHY_CONFIG_DELAY                ((uint32_t)0x00000FFF)
+
+ #define PHY_READ_TO                     ((uint32_t)0x0000FFFF)
+ #define PHY_WRITE_TO                    ((uint32_t)0x0000FFFF)
+
+ /* Section 3: Common PHY Registers */
+
+ #define PHY_BCR                         ((uint16_t)0x00)    /*!< Transceiver Basic Control Register   */
+ #define PHY_BSR                         ((uint16_t)0x01)    /*!< Transceiver Basic Status Register    */
+
 #endif /* STM32F4XX_HAL_CONF_H_ */

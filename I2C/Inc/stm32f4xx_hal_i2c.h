1625e3f20623b09753a303bda4e717974e3e12b0
Shriyans S Sahoo <shriyans.s.sahoo@gmail.com>
2026-09-15 07:49:48 +0530

    started i2c declaration

---
+++ I2C/Inc/stm32f4xx_hal_i2c.h
@@ -0,0 +1,48 @@
+/*
+ * stm32f4xx_hal_i2c.h
+ *
+ *  Created on: Sep 15, 2026
+ *      Author: HP
+ */
+
+#ifndef STM32F4XX_HAL_I2C_H_
+#define STM32F4XX_HAL_I2C_H_
+
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal_def.h"
+
+/** @addtogroup STM32F4xx_HAL_Driver
+  * @{
+  */
+
+/** @addtogroup I2C
+  * @{
+  */
+
+/* Exported types ------------------------------------------------------------*/
+/** @defgroup I2C_Exported_Types I2C Exported Types
+  * @{
+  */
+
+
+/**
+  * @}
+  */
+
+/* Exported constants --------------------------------------------------------*/
+/** @defgroup I2C_Exported_Constants I2C Exported Constants
+  * @{
+  */
+
+/** @defgroup I2C_Error_Code I2C Error Code
+  * @brief    I2C Error Code
+  * @{
+  */
+
+
+#endif /* STM32F4XX_HAL_I2C_H_ */
